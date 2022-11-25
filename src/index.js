64c9424442dacfba10d0f7f8b6a9803a0306f1d5
Sinan <haxsmith77@gmail.com>
2022-11-26 03:21:17 +1100

    Updates to index.js

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ const mongoose = require('mongoose');
 const Comment = require('./models/comments')
 const dotenv = require('dotenv').config({path: './config.env'})
 
-mongoose.connect(process.env.DATABASE_LOGIN, () => {
-    console.log("Connected to database.");
-});
+mongoose.connect(process.env.DATABASE_LOGIN, {dbName: "sample_mflix"});
 
 app.get('/comments', (req, res) => {
   Comment.find().then((response) => {
       res.send(response)
   })
+})
+
+app.get('/', async (req, res) => {
+  const a = await Comment.create({"name": "Steve", "email": "stevehenkerton@gmail.com"})
+  console.log(a)
+  res.send(a)
 })
 
 app.listen(port, () => {
