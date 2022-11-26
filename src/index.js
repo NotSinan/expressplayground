d8a09fe88bac1203d6765dfabdec4c9d705d7590
Sinan <haxsmith77@gmail.com>
2022-11-26 11:55:17 +1100

    Updated index.js to use middleware

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
+const { urlencoded } = require('express');
 const express = require('express')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose');
-const Comment = require('./models/comments')
 const dotenv = require('dotenv').config({path: './config.env'})
+const comments = require('./api/comments')
 
 mongoose.connect(process.env.DATABASE_LOGIN, {dbName: "sample_mflix"});
-
-app.get('/comments', (req, res) => {
-  Comment.find().then((response) => {
-      res.send(response)
-  })
-})
-
-app.get('/', async (req, res) => {
-  const a = await Comment.create({"name": "Steve", "email": "stevehenkerton@gmail.com"})
-  console.log(a)
-  res.send(a)
-})
+app.use(express.json())
+app.use(urlencoded({extended: false}))
+app.use('/', comments)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
