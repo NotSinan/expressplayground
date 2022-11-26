59b480104706cc0a0ad8aa60fc1a93c0e4211b21
Sinan <haxsmith77@gmail.com>
2022-11-26 15:43:44 +1100

    Created movie schema

---
+++ src/models/movies.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+    plot: String,
+    genre: [String],
+    runtime: Number,
+    cast: [String],
+    poster: String,
+    title: String,
+    fullplot: String,
+    languages: [String],
+    released: Date,
+    directors: [String],
+    rated: String,
+    year: Number,
+    countries: [String]
+})
+
+const Movie = mongoose.model('movie', movieSchema)
+
+module.exports = Movie;
